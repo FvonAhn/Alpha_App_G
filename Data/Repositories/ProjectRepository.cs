61955fb5038f2ab969e387ec7bb8022577a408a7
Fredrik <fredd24hate@gmail.com>
2025-04-16 23:55:17 +0200

    fixed repositories for user and projects.

--- Data/Repositories/ProjectRepository.cs
+++ Data/Repositories/ProjectRepository.cs
@@ -1,13 +1,109 @@
-﻿namespace Data.Repositories
+﻿using Data.Context;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System.Diagnostics;
+namespace Data.Repositories
 {
-    public class ProjectRepository
+    public class ProjectRepository(DataContext context)
     {
+        private readonly DataContext _context = context;
+
         // CREATE
+
+        public virtual async Task<ProjectEntity> CreateProjectAsync(ProjectEntity project)
+        {
+            try
+            {
+                await _context.AddAsync(project);
+                await _context.SaveChangesAsync();
+                return project;
+            }
+            catch (Exception ex) 
+            {
+                Debug.WriteLine(ex.Message);
+                return null!;
+            }
+        }
 
         // READ
 
+        public virtual async Task<IEnumerable<ProjectEntity>> GetAllProjectsAsync()
+        {
+            try
+            {
+                return await _context.Projects.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return [];
+            }
+        }
+
+        public virtual async Task<ProjectEntity?> GetProjectByIdAsync(int id)
+        {
+            try
+            {
+                return await _context.Projects.FirstOrDefaultAsync(x => x.Id == id);
+            }
+            catch (Exception ex) 
+            {
+                Debug.WriteLine(ex.Message);
+                return null!;
+            }
+        }
+
         // UPDATE
 
+        public virtual async Task<bool> UpdateProjectAsync(ProjectEntity updatedProject)
+        {
+            var existingProject = await _context.Projects.FirstOrDefaultAsync(x => x.Id == updatedProject.Id);
+            if (existingProject == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                existingProject!.ProjectName = updatedProject.ProjectName;
+                existingProject.ClientName = updatedProject.ClientName;
+                existingProject.Description = updatedProject.Description;
+                existingProject.StartDate = updatedProject.StartDate;
+                existingProject.EndDate = updatedProject.EndDate;
+                existingProject.Budget = updatedProject.Budget;
+
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex) 
+            {
+                Debug.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
         // DELETE
+
+        public virtual async Task<bool> DeleteProjectAsync(int id)
+        {
+            var existingProject = _context.Projects.FirstOrDefault(x => x.Id == id);
+            if (existingProject == null) 
+            {
+                return false; 
+            }
+
+            try
+            {
+                _context.Projects.Remove(existingProject!);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return false;
+            }
+        }
     }
 }
--- Data/Repositories/UserRepository.cs
+++ Data/Repositories/UserRepository.cs
@@ -1,13 +1,106 @@
-﻿namespace Data.Repositories
+﻿using Data.Context;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace Data.Repositories
 {
-    public class UserRepository
+    public class UserRepository(DataContext context)
     {
+        private readonly DataContext _context = context;
+
         // CREATE
+
+        public virtual async Task<UserEntity> CreateUserAsync(UserEntity entity)
+        {
+            try
+            {
+                await _context.AddAsync(entity);
+                await _context.SaveChangesAsync();
+                return entity;
+            }
+            catch (Exception ex) 
+            {
+                Debug.WriteLine(ex.Message);
+                return null!;
+            }
+        }
 
         // READ
 
+        public virtual async Task<IEnumerable<UserEntity>> GetAllUsersAsync()
+        {
+            try
+            {
+                return await _context.Users.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return []; 
+            }
+        }
+
+        public virtual async Task<UserEntity?> GetUserById(int id)
+        {
+            try
+            {
+                return await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null!;
+            }
+        }
+
         // UPDATE
 
+        public virtual async Task<bool> UpdateUserAsync(UserEntity updatedUser)
+        {
+            var existingUser = await _context.Users.FirstOrDefaultAsync(x => x.Id == updatedUser.Id);
+            if (existingUser == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                existingUser.FullName = updatedUser.FullName;
+                existingUser.Email = updatedUser.Email;
+                existingUser.Password = updatedUser.Password;
+
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex) 
+            {
+                Debug.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
         // DELETE
+
+        public virtual async Task<bool> DeleteUserAsync(UserEntity deletedUser)
+        {
+            var existingUser = _context.Users.FirstOrDefault(x => x.Id == deletedUser.Id);
+            if (existingUser == null) 
+            {
+                return false;
+            }
+
+            try
+            {
+                _context.Users.Remove(existingUser);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex) 
+            {
+                Debug.WriteLine(ex.Message);
+                return false;
+            }
+        }
     }
 }
